ca6efbe63a280cf3e88b4d45607f6f040c1fbc3f
shizuo-kaji <shizuo.kaji@gmail.com>
2025-01-08 16:58:15 +0900

    Test: build workflow

--- src/coboundary_enumerator.cpp
+++ src/coboundary_enumerator.cpp
@@ -34,144 +34,60 @@ bool CoboundaryEnumerator::hasNextCoface() {
 	auto cx = cube.x();
 	auto cy = cube.y();
 	auto cz = cube.z();
+	const int8_t offsets0[][4] = {{1,1,2,2}, {1,1,0,2}, {1,2,1,1}, {1,0,1,1}, {2,1,1,0}, {0,1,1,0}};
 	switch (dim) {
 		case 0: // dim0
-		for (uint8_t i = position; i < 6; ++i) {
-			switch (i){
-			case 0:
-				birth = max(cube.birth, dcg->dense3[cx+1][cy+1][cz+2]);
-				nextCoface = Cube(birth, cx, cy, cz, 2);
-				break;
+			for (uint8_t i = position; i < 6; ++i) {
+				int x = cx + offsets0[i][0];
+				int y = cy + offsets0[i][1];
+				int z = cz + offsets0[i][2];
+				int m = offsets0[i][3];
+				
+				birth = max(cube.birth, dcg->dense3[x][y][z]);
+				nextCoface = Cube(birth, cx + (m == 0 ? i/4 - 1 : 0), 
+										cy + (m == 1 ? i/2 - 1 : 0), 
+										cz + (m == 2 ? 1 - i%2 : 0), m);
 
-			case 1:
-				birth = max(cube.birth, dcg->dense3[cx+1][cy+1][cz]);
-				nextCoface = Cube(birth, cx, cy, cz - 1, 2);
-				break;
-
-			case 2:
-				birth = max(cube.birth, dcg->dense3[cx+1][cy + 2][cz+1]);
-				nextCoface = Cube(birth, cx, cy, cz, 1);
-				break;
-
-			case 3:
-				birth = max(cube.birth, dcg->dense3[cx+1][cy][cz+1]);
-				nextCoface = Cube(birth, cx, cy - 1, cz, 1);
-				break;
-
-			case 4:
-				birth = max(cube.birth, dcg->dense3[cx + 2][cy+1][cz+1]);
-				nextCoface = Cube(birth, cx, cy, cz, 0);
-				break;
-
-			case 5:
-				birth = max(cube.birth, dcg->dense3[cx][cy+1][cz+1]);
-				nextCoface = Cube(birth, cx - 1, cy, cz, 0);
-				break;
+				if (birth != dcg->threshold) {
+					position = i + 1;
+					return true;
+				}
 			}
-
-			if (birth != dcg->threshold) {
-				position = i + 1;
-				return true;
-			}
-		}
-		return false;
+			return false;
 
 		case 1: // dim1
-		switch (cube.m()) {
-			case 0: // dim1 type0 (x-axis -> )
-			for(uint8_t i = position; i < 4; ++i){
-				switch(i){
-				case 0:
-					birth = max(max(cube.birth, dcg->dense3[cx+1][cy+1][cz + 2]), dcg->dense3[cx + 2][cy+1][cz + 2]);
-					nextCoface = Cube(birth, cx, cy, cz, 1);
-					break;
-
-				case 1:
-					birth = max(max(cube.birth, dcg->dense3[cx+1][cy+1][cz]), dcg->dense3[cx + 2][cy+1][cz]);
-					nextCoface = Cube(birth, cx, cy, cz - 1, 1);
-					break;
-
-				case 2:
-					birth = max(max(cube.birth, dcg->dense3[cx+1][cy + 2][cz+1]), dcg->dense3[cx + 2][cy + 2][cz+1]);
-					nextCoface = Cube(birth, cx, cy, cz, 0);
-					break;
-
-				case 3:
-					birth = max(max(cube.birth, dcg->dense3[cx+1][cy][cz+1]), dcg->dense3[cx + 2][cy][cz+1]);
-					nextCoface = Cube(birth, cx, cy - 1, cz, 0);
-					break;
+			switch (cube.m()) {
+				case 0: { // dim1 type0 (x-axis -> )
+					const int8_t offsets[][4] = {{1,1,2,1}, {1,1,0,1}, {1,2,1,0}, {1,0,1,0}};
+					for(uint8_t i = position; i < 4; ++i){
+						birth = max(max(cube.birth, dcg->dense3[cx+offsets[i][0]][cy+offsets[i][1]][cz+offsets[i][2]]), dcg->dense3[cx+2][cy+offsets[i][1]][cz+offsets[i][2]]);
+						nextCoface = Cube(birth, cx, cy - (i==3), cz - (i==1), offsets[i][3]);
+						if (birth != dcg->threshold) { position = i + 1; return true; }
+					}
+					return false;
 				}
 
-				if (birth != dcg->threshold) {
-					position = i + 1;
-					return true;
-				}
-			}
-			return false;
-
-			case 1: // dim1 type1 (y-axis -> )
-			for(uint8_t i = position; i < 4; ++i){
-				switch(i){
-				case 0:
-					birth = max(max(cube.birth, dcg->dense3[cx+1][cy+1][cz + 2]), dcg->dense3[cx+1][cy + 2][cz + 2]);
-					nextCoface = Cube(birth, cx, cy, cz, 2);
-					break;
-
-				case 1:
-					birth = max(max(cube.birth, dcg->dense3[cx+1][cy+1][cz]), dcg->dense3[cx+1][cy + 2][cz]);
-					nextCoface = Cube(birth, cx, cy, cz - 1, 2);
-					break;
-
-				case 2:
-					birth = max(max(cube.birth, dcg->dense3[cx + 2][cy+1][cz+1]), dcg->dense3[cx + 2][cy + 2][cz+1]);
-					nextCoface = Cube(birth, cx, cy, cz, 0);
-					break;
-
-				case 3:
-					birth = max(max(cube.birth, dcg->dense3[cx][cy+1][cz+1]), dcg->dense3[cx][cy + 2][cz+1]);
-					nextCoface = Cube(birth, cx - 1, cy, cz, 0);
-					break;
+				case 1: { // dim1 type1 (y-axis -> )
+					const int8_t offsets[][4] = {{1,1,2,2}, {1,1,0,2}, {2,1,1,0}, {0,1,1,0}};
+					for(uint8_t i = position; i < 4; ++i){
+						birth = max(max(cube.birth, dcg->dense3[cx+offsets[i][0]][cy+offsets[i][1]][cz+offsets[i][2]]), dcg->dense3[cx + offsets[i][0]][cy+2][cz + offsets[i][2]]);
+						nextCoface = Cube(birth, cx - (i==3), cy, cz - (i==1), offsets[i][3]);
+						if (birth != dcg->threshold) { position = i + 1; return true; }
+					}
+					return false;
 				}
 
-				if (birth != dcg->threshold) {
-					position = i + 1;
-					return true;
+				case 2: { // dim1 type2 (z-axis -> )
+					const int8_t offsets[][4] = {{1,2,1,2}, {1,0,1,2}, {2,1,1,1}, {0,1,1,1}};
+					for(uint8_t i = position; i < 4; ++i){
+						birth = max(max(cube.birth, dcg->dense3[cx+offsets[i][0]][cy+offsets[i][1]][cz+offsets[i][2]]), dcg->dense3[cx + offsets[i][0]][cy+offsets[i][1]][cz + 2]);
+						nextCoface = Cube(birth, cx - (i==3), cy - (i==1), cz, offsets[i][3]);
+						if (birth != dcg->threshold) { position = i + 1; return true; }
+					}
+					return false;
 				}
 			}
 			return false;
-
-			case 2: // dim1 type2 (z-axis -> )
-			for(uint8_t i = position; i < 4; ++i){
-				switch(i){
-					case 0:
-						birth = max(max(cube.birth, dcg->dense3[cx+1][cy + 2][cz+1]), dcg->dense3[cx+1][cy + 2][cz + 2]);
-						nextCoface = Cube(birth, cx, cy, cz, 2);
-						break;
-
-					case 1:
-						birth = max(max(cube.birth, dcg->dense3[cx+1][cy][cz+1]), dcg->dense3[cx+1][cy][cz + 2]);
-						nextCoface = Cube(birth, cx, cy - 1, cz, 2);
-						break;
-
-					case 2:
-						birth = max(max(cube.birth, dcg->dense3[cx + 2][cy+1][cz+1]), dcg->dense3[cx + 2][cy+1][cz + 2]);
-						nextCoface = Cube(birth, cx, cy, cz, 1);
-						break;
-
-					case 3:
-						birth = max(max(cube.birth, dcg->dense3[cx][cy+1][cz+1]), dcg->dense3[cx][cy+1][cz + 2]);
-						nextCoface = Cube(birth, cx - 1, cy, cz, 1);
-						break;
-				}
-
-				if (birth != dcg->threshold) {
-					position = i + 1;
-					return true;
-				}
-			}
-			return false;
-		}
-		return false;
 
 		case 2: // dim2
 		switch (cube.m()) {
